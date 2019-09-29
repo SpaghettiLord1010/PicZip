43139fbd6cb7ab21fc6c27d6c814b92a8f9aefd3
SpaghettiLord1010 <50876603+SpaghettiLord1010@users.noreply.github.com>
2019-09-29 16:33:56 +0200

    Feel free to use in any way - credit would be nice
    Yeah, use it as you wish, if you use it in another project, credit would be nice, thank you!

---
+++ PicZip/PicZip/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace PicZip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Input Zip File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(12, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 95);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select Hiding Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(686, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 510);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(12, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 95);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start PICZIPPING";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1214, 534);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ PicZip/PicZip/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PicZip
+{
+    public partial class Form1 : Form
+    {
+
+        String ZipPath = "ZipPath";
+        String PicPath = "PicPath";
+        String ZipPickPath = "ZipPickPath";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //check if Button should be enabled
+            if (ZipPath != "ZipPath" & PicPath != "PicPath")
+            {
+                button3.Enabled = true;
+                Console.Write("Button Enabled!");
+            }
+            else
+            {
+                button3.Enabled = false;
+                Console.Write("Button Disabled!");
+            }
+            pictureBox1.ImageLocation = PicPath;
+        }
+
+        public void EnableButton()
+            {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Please Select a Picture File!";
+            openFileDialog1.ShowDialog();
+            PicPath = openFileDialog1.FileName;
+
+
+//check if Button should be enabled
+            if (ZipPath != "ZipPath" & PicPath != "PicPath")
+            {
+                button3.Enabled = true;
+                Console.Write("Button Enabled!");
+            }
+            else
+            {
+                button3.Enabled = false;
+                Console.Write("Button Disabled!");
+            }
+            pictureBox1.ImageLocation = PicPath;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Please Select a ZIP-File!";
+            openFileDialog1.ShowDialog();
+            ZipPath = openFileDialog1.FileName;
+            Console.WriteLine("wooorks");
+
+//check if Button should be enabled
+            if (ZipPath != "ZipPath" & PicPath != "PicPath")
+            {
+                button3.Enabled = true;
+                Console.Write("Button Enabled!");
+            }
+            else
+            {
+                button3.Enabled = false;
+                Console.Write("Button Disabled!");
+            }
+            pictureBox1.ImageLocation = PicPath;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            String PicExtension = "";
+            PicExtension = Path.GetExtension(PicPath);
+            saveFileDialog1.Title = "Please select the output directory and name - Save with the extension "+PicExtension+"!";
+            saveFileDialog1.ShowDialog();
+            saveFileDialog1.DefaultExt = PicExtension;
+            ZipPickPath = saveFileDialog1.FileName;
+            
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C copy /b "+PicPath+" + "+ ZipPath+" "+ZipPickPath;
+            Console.WriteLine(startInfo.Arguments);
+            process.StartInfo = startInfo;
+            process.Start();
+
+
+        }
+    }
+}
